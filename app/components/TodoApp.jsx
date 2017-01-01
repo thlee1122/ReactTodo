275b0695d32197cf6649bbd4b31b23247001de1c
Tae Hoon Lee <thlee1122@hotmail.com>
2016-12-31 22:05:01 -0500

    Add static todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,33 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+	getInitialState: function() {
+		return {
+			todos: [
+				{
+					id: 1,
+					text: 'walk the dog'
+				}, {
+					id: 2,
+					text: 'Clean the yard'
+				}, {
+					id: 3,
+					text: 'Leave mail on porch'
+				}, {
+					id: 4,
+					text: 'Play video games'
+				}
+			]
+		};
+	},
+
 	render: function() {
+		var {todos} = this.state;
+
 		return (
 			<div>
-				TodoApp.jsx
+				<TodoList todos={todos}/>
 			</div>
 		)
 	}
---
+++ app/components/TodoList.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+	render: function() {
+		var {todos} = this.props;
+		var renderTodos = () => {
+			return todos.map((todo) => {
+				return (
+					//{...todo} will spread through the 'todo' object and find the properties in that object
+					//with this you will be able to use 'this.props' and get 'text' property of the 'todo' object
+					<Todo key={todo.id} {...todo}/>
+				)
+			})
+		};
+
+		return (
+			<div>
+				{renderTodos()}
+			</div>
+		)
+	}
+});
+
+module.exports = TodoList;
