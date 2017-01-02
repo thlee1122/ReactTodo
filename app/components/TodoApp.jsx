34b4b39ecfc950506c77cc2595c009b655c6ab81
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-01 23:33:06 -0500

    Modify TodoApp function

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
 	getInitialState: function() {
@@ -10,16 +11,16 @@ var TodoApp = React.createClass({
 			searchText: '',
 			todos: [
 				{
-					id: 1,
+					id: uuid(),
 					text: 'walk the dog'
 				}, {
-					id: 2,
+					id: uuid(),
 					text: 'Clean the yard'
 				}, {
-					id: 3,
+					id: uuid(),
 					text: 'Leave mail on porch'
 				}, {
-					id: 4,
+					id: uuid(),
 					text: 'Play video games'
 				}
 			]
@@ -27,7 +28,15 @@ var TodoApp = React.createClass({
 	},
 
 	handleAddTodo: function(text) {
-		alert('new todo:' + text)
+		this.setState({
+			todos: [
+				...this.state.todos,
+				{
+					id: uuid(), 
+					text: text
+				}
+			]
+		})
 	},
 
 	handleSearch: function(showCompleted, searchText) {
--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -8,22 +8,21 @@ var TodoList = require('TodoList');
 var Todo = require('Todo');
 
 describe('TodoList', () => {
-	it('should exist', () => {
-		expect(TodoList).toExist();
-	})
+  it('should exist', () => {
+    expect(TodoList).toExist();
+  });
 
-	it('should render one Todo component for each todo item', () => {
-		var todos = [{
-			id: 1,
-			text: "Do something"
-		}, {
-			id: 2,
-			text: 'Check mail'
-		}]
+  it('should render one Todo component for each todo item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Do something'
+    }, {
+      id: 2,
+      text: 'Check mail'
+    }];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
 
-		var todoList = TestUtils.renderIntoDocument(<TodoList todo={todos}/>);
-		var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
-
-		expect(todosComponents.length).toBe(todos.length);
-	})
-})
+    expect(todosComponents.length).toBe(todos.length);
+  });
+});
